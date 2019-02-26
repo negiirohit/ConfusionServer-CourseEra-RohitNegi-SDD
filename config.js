ea3cc05bc97fbf28987c78cadb907e0dccce7aaa
Rohit Negi <rohitnegi@smartdatainc.net>
2019-02-26 13:51:48 +0530

    confusion server course era

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://sdirectdb:sdirectdb@192.168.0.5/sdirectdb',
+    'facebook': {
+        clientId: 'rohitnegi',
+        clientSecret: 'mysecret'
+    }
+}
+
+
+
+
+
+
+
+
+
+ 
