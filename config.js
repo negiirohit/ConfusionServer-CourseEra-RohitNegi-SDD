4bc509104c4a52e27be129b65f66ba5f3e9969be
negiirohit <47972587+negiirohit@users.noreply.github.com>
2019-02-26 13:54:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl' : 'mongodb://sdirectdb:sdirectdb@192.168.0.5/sdirectdb',
     'facebook': {
-        clientId: 'rohitnegi',
-        clientSecret: 'mysecret'
+        clientId: 'demoId674326238',
+        clientSecret: 'demoSecret567687'
     }
 }
 
